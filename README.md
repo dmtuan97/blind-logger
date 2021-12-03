c3009fe28572cd69ba8115f27edfb80dd4bf71da
Tuan Doan <tuan.doan1@mservice.com.vn>
2021-12-03 14:21:50 +0700

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+1. Copy Logger to your source
+2. Fill your_ip_address
+3. npm install
+
+4. Put `Logger.log(data)` in your code
---
+++ pre-install/Logger.js
@@ -0,0 +1,15 @@
+export const log = (data) => {
+  const response = window.fetch("http://<your_ip_address>:3000/log", {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify({ value: data }), // body data type must match "Content-Type" header
+  });
+};
